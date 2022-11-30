ed8e95562ca517a7cb4c46f0cac6452c2287a2f0
lvieira268 <lvieira268@gmail.com>
2022-11-30 13:52:26 -0300

    init app with components
    Signed-off-by: lvieira268 <lvieira268@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Home } from './pages/Home'
+import { UserForm } from './pages/UserForm'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Home />
+      <UserForm />
+    </div>
+  )
+}
+
+export default App
